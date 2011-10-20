1df52315942c7e2fe6115cca9f339afe23e89153
Michael-Keith Bernard <mk@bionicpandagames.com>
2011-10-20 10:20:20 -0700

    Will now create a backups directory if it doesn't currently exist.

--- build.sh
+++ build.sh
@@ -9,9 +9,14 @@
 echo "== Installing ~/.bashrc & other related dot files..."
 this_dir=$(pwd)
 
+echo "== Backing up old .bashrc"
+if [ ! -d "backups/" ]; then
+  mkdir backups
+fi
+
 # Backup the existing 
 now=$(date +%s)
-cp ~/.bashrc ~/bashrc/backups/bashrc$now.old
+cp ~/.bashrc backups/bashrc$now.old
 
 # Assemble bashrc from parts
 cat default.sh > ~/.bashrc
